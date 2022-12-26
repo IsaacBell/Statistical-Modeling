794b5dd3ab76a991e06c23e18737767e8dd463b7
= <=>
2022-12-25 20:49:06 -0600

    wip

--- app/engine/engine.hpp
+++ app/engine/engine.hpp
@@ -2,28 +2,32 @@
 #include <memory>
 
 #include "../common/constants.hpp"
+#include "../queues/message/producer.hpp"
 
-namespace TPF {
-  enum State : int16_t {
-    DISCONNECTED = 0,           // initial
+namespace TPF
+{
+  enum State : int16_t
+  {
+    DISCONNECTED = 0, // initial
     CONNECTING,
-    CONNECT_ACK,                // ctp: front end is  connected; tap:  logged in
+    CONNECT_ACK, // ctp: front end is  connected; tap:  logged in
     AUTHENTICATING,
-    AUTHENTICATE_ACK,           // ctp: trade authencated
+    AUTHENTICATE_ACK, // ctp: trade authencated
     LOGGING_IN,
-    LOGIN_ACK,                  // ctp: logged in, t:ap api ready
+    LOGIN_ACK, // ctp: logged in, t:ap api ready
     LOGGING_OUT,
     STOPPED
   };
 
   // Interface class: base engine for td and md engine
-  class Engine {
+  class Engine
+  {
   public:
     // static mutex sendlock_;  // msg send lock_
     // static std::unique_ptr<CMsgq> msgq_send_;  //for md and td messenge to client, all engine share same msgq, usually publish mode
     // std::unique_ptr<CMsgq> msgq_recv_;  //each engine has its own msgq, usually subscribe mode
     std::atomic<State> state_;
-    std::unique_ptr<Messenger> messenger_;
+    std::unique_ptr<Producer> producer_;
 
     Engine();
     virtual ~Engine();
