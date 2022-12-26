659e09fedac630c55316921209578a44ea6b1be0
= <=>
2022-12-26 12:55:00 -0600

    wip

--- app/services/trading_engine_service.hpp
+++ app/services/trading_engine_service.hpp
@@ -1,18 +1,3 @@
-/*****************************************************************************
- * Copyright [2019]
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *****************************************************************************/
-
 #ifndef _TPF_SERVICES_TRADING_ENGINE_SERVICE_H_
 #define _TPF_SERVICES_TRADING_ENGINE_SERVICE_H_
 
@@ -48,12 +33,8 @@ namespace TPF
     // BROKERS _broker = BROKERS::PAPER;
     std::vector<std::thread *> p_threads_;
     // std::vector<std::shared_ptr<Engine>> p_engines_;
-    // std::unique_ptr<IMessenger> msg_relay_;
-    // std::shared_ptr<SQLogger> logger;
 
   public:
-    // std::atomic<bool>* setconsolecontrolhandler(void);
-    // setconsolecontrolhandler(void)
     void start_engine(shared_ptr<Engine> p_engine);
     int32_t cronjobs(bool force = true);
 
