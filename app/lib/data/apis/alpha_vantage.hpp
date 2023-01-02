38be899d9351d9092318f20eb28167412dec5ce4
= <=>
2023-01-01 20:52:16 -0600

    wip

--- app/lib/data/apis/alpha_vantage.hpp
+++ app/lib/data/apis/alpha_vantage.hpp
@@ -1,18 +1,25 @@
 #ifndef _TPF_LIB_DATA_APIS_ALPHA_VANTAGE_HPP_
 #define _TPF_LIB_DATA_APIS_ALPHA_VANTAGE_HPP_
 
-#include "../api.hpp";
+#include <cpr>
+
+#include "../request_data.hpp"
 
 namespace TPF
 {
-  struct AlphaVantageAPI : API
+  struct AlphaVantageAPI
   {
   public:
+    AlphaVantageAPI::AlphaVantageAPI(APIRequestData &data);
+
     bool GetHistoricalData();
 
   private:
     const std::string url() const;
     const std::string api_key() const;
+
+    bool Get(std::string_view url = "", cpr::Parameters params = nullptr);
+    bool Post(std::string_view url = "", cpr::Parameters params = nullptr);
   };
 }
 
